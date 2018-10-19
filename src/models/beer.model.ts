f9c4f326c341953d3c05fab11c4ff8a2479da0c9
Brandon Jernigan <brandon@whatisboom.com>
2018-10-19 01:49:31 +0000

    Added style to beer.model

--- src/models/beer.model.ts
+++ src/models/beer.model.ts
@@ -3,6 +3,7 @@ import { model, Schema } from "mongoose";
 const BeerSchema = new Schema({
   name: { type: String, required: true },
   abv: Number,
+  style: String,
   brewery: { type: Schema.Types.ObjectId, ref: 'brewery' }
 }, {
   versionKey: false
