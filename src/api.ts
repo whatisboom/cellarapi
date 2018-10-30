ed15f73c6d0a7a400615da5caa8a5e0b5a2d258c
Brandon Jernigan <bjernigan@netspend.com>
2018-10-30 14:06:21 -0500

    updated node version, added helmet

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
 import {
   genericErrorHandler,
   jwtErrorHandler,
@@ -9,7 +10,7 @@ import {
 } from './middleware';
 
 const api = express();
-
+api.use(helmet());
 api.use(cors());
 api.use(compression());
 api.use(bodyParser.urlencoded({ extended: true }));
