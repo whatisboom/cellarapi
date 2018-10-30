47df33a0d77232be88bbd2e279758475a08f7c3f
Brandon Jernigan <bjernigan@netspend.com>
2018-10-30 17:47:56 -0500

    rebase origin/master into feature/refresh-tokens

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,7 @@ import * as cors from 'cors';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
-import {
-  genericErrorHandler,
-  jwtErrorHandler,
-  jwtValidator
-} from './middleware';
+import { jwtErrorHandler, jwtValidator } from './middleware';
 
 const api = express();
 api.use(helmet());
@@ -18,6 +14,5 @@ api.use(bodyParser.json());
 
 api.use(jwtValidator);
 api.use(jwtErrorHandler);
-api.use(genericErrorHandler);
 
 export default api;
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,3 +2,4 @@ export * from './allowOwnProfile';
 export * from './checkPermissions';
 export * from './genericErrorHandler';
 export * from './jwtValidator';
+export * from './validationErrorHandler';
