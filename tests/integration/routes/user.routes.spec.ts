ceaeb89303ced7f70e9bba1fbfa99d96545719e0
Brandon Jernigan <bjernigan@netspend.com>
2018-11-02 13:55:58 -0500

    user and auth integration tests

--- tests/integration/routes/user.routes.spec.ts
+++ tests/integration/routes/user.routes.spec.ts
@@ -1,6 +1,8 @@
 import api from '../../../src/api';
 import * as supertest from 'supertest';
 import { getValidJwt, test_user } from '../../utils';
+import { OwnedModel } from '../../../src/models/quantity.model';
+import BeerModel from '../../../src/models/beer.model';
 
 const jwt: string = getValidJwt();
 
@@ -52,6 +54,17 @@ describe('Users Routes', () => {
   });
 
   describe('GET /users/me', () => {
+    it('should 404 on unknown user', done => {
+      supertest(api)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(404)
+        .end((err: Error, response: supertest.Response) => {
+          if (err) done(err);
+          expect(response.body).toHaveProperty('error');
+          done();
+        });
+    });
     it('should return a single user', done => {
       const meJWT = getValidJwt(test_user);
       supertest(api)
@@ -100,13 +113,63 @@ describe('Users Routes', () => {
     });
   });
 
+  describe('POST /users/:userId/beers', () => {
+    it('should send a 404 with an unknown beer', done => {
+      const beerId = '1234';
+      supertest(api)
+        .post(`/users/${test_user._id}/beers`)
+        .send({
+          beerId
+        })
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(404)
+        .end((err: Error, response: supertest.Response) => {
+          if (err) done(err);
+          expect(response.body).toHaveProperty('error');
+          done();
+        });
+    });
+    it('should add a beer when it does not exist for the user', async done => {
+      let beer;
+      // create a beer
+      supertest(api)
+        .post('/beers')
+        .send({
+          name: 'Test Beer'
+        })
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(200)
+        .end((err: Error, response: supertest.Response) => {
+          if (err) done(err);
+          expect(response.body).toHaveProperty('beer');
+          beer = response.body.beer;
+
+          supertest(api)
+            .post(`/users/${test_user._id}/beers`)
+            .set('Authorization', `Bearer ${jwt}`)
+            .send({
+              beer: beer._id,
+              amount: 1
+            })
+            .expect(200)
+            .end((err: Error, response: supertest.Response) => {
+              if (err) done(err);
+              expect(response.body).toHaveProperty('user');
+              expect(response.body.user).toHaveProperty('owned');
+              expect(response.body.user.owned).toHaveLength(1);
+              done();
+            });
+        });
+    });
+  });
+
   describe('DELETE /users/:userId', () => {
     it('should successfully remove the user', done => {
       supertest(api)
         .delete(`/users/${test_user._id}`)
         .set('Authorization', `Bearer ${jwt}`)
         .expect(204)
-        .end((err: Error, response: supertest.Response) => {
+        .end(async (err: Error, response: supertest.Response) => {
           if (err) done(err);
           done();
         });
@@ -124,5 +187,20 @@ describe('Users Routes', () => {
     });
   });
 
-  // add POST /users/:userId/beers
+  describe('post-cleanup', () => {
+    it('should return a zero length users array', async done => {
+      await OwnedModel.deleteMany({});
+      await BeerModel.deleteMany({});
+      supertest(api)
+        .get('/users')
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(200)
+        .end((err: Error, response: supertest.Response) => {
+          if (err) done(err);
+          expect(response.body).toHaveProperty('users');
+          expect(response.body.users).toHaveLength(0);
+          done();
+        });
+    });
+  });
 });
