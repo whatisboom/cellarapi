f665c75dfcc901ca6ad820ff00b436c72c4113cd
Brandon Jernigan <bjernigan@netspend.com>
2018-11-09 09:56:03 -0600

    refactored db connection

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { readdirSync } from 'fs';
-import * as path from 'path';
 import api from './api';
+import db from './db';
 
 const port: string = process.env.PORT || '8000';
 
 const ENV = process.env.NODE_ENV;
 if (ENV !== 'test' && ENV !== 'ci') {
-  api.listen(port, (err: Error) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    console.log(`server is listening on ${port}`);
+  db(ENV).then(() => {
+    api.listen(port, (err: Error) => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+      console.log(`server is listening on ${port}`);
+    });
   });
 }
