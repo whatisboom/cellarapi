28bd79d32e8d90c03d95e6d8b3efe86725d500e0
Brandon Jernigan <brandon@whatisboom.com>
2018-11-24 23:18:44 -0600

    updateds middleware errors and error tests

--- tests/unit/errors/ConflictError.spec.ts
+++ tests/unit/errors/ConflictError.spec.ts
@@ -4,4 +4,16 @@ describe('ConflictError', () => {
   it('should have its message set to "conflict"', () => {
     expect(new ConflictError().message).toEqual('conflict');
   });
+  it('should be an error instance', () => {
+    expect(new ConflictError() instanceof Error).toBe(true);
+  });
+  it('should return 409 when no error code is passed', () => {
+    expect(new ConflictError().status).toBe(409);
+  });
+  it('should return a custom error code when passed', () => {
+    expect(new ConflictError('message', 408).status).toBe(408);
+  });
+  it('should return the custom error message', () => {
+    expect(new ConflictError('test message').message).toBe('test message');
+  });
 });
--- tests/unit/errors/UnauthorizedError.spec.ts
+++ tests/unit/errors/UnauthorizedError.spec.ts
@@ -4,4 +4,16 @@ describe('UnauthorizedError', () => {
   it('should have its message set to "conflict"', () => {
     expect(new UnauthorizedError().message).toEqual('unauthorized');
   });
+  it('should be an error instance', () => {
+    expect(new UnauthorizedError() instanceof Error).toBe(true);
+  });
+  it('should return 409 when no error code is passed', () => {
+    expect(new UnauthorizedError().status).toBe(401);
+  });
+  it('should return a custom error code when passed', () => {
+    expect(new UnauthorizedError('message', 408).status).toBe(408);
+  });
+  it('should return the custom error message', () => {
+    expect(new UnauthorizedError('test message').message).toBe('test message');
+  });
 });
