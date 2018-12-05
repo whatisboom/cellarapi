1861f9cf6cfa8e8c1a89ef6091ebb51d5ab79e12
Brandon Jernigan <bjernigan@netspend.com>
2018-12-05 11:52:11 -0600

    added loader.io html verification endpoint

--- src/routes/base.routes.ts
+++ src/routes/base.routes.ts
@@ -4,4 +4,7 @@ export default function baseRoutes(api: Router): void {
   api.route('/').get((req, res) => {
     res.status(200).send('🤙');
   });
+  api.route('/loaderio-a44aad14bfb7bb1081e8ac42fe371a62').get((req, res) => {
+    res.send('loaderio-a44aad14bfb7bb1081e8ac42fe371a62');
+  });
 }
