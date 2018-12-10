fd57ad72881853255a0b1a1e947057fb568f8aed
Brandon Jernigan <brandon@whatisboom.com>
2018-12-09 20:15:45 -0600

    added auth/oauth/untappd route and oauth method to auth ctrl (#44)

--- src/middleware/jwtValidator.ts
+++ src/middleware/jwtValidator.ts
@@ -6,7 +6,7 @@ export function jwtValidator(JWT_SECRET: string): RequestHandler {
   return jwt({
     secret: JWT_SECRET
   }).unless({
-    path: ['/', '/auth/signin', '/auth/signup', /^\/loaderio/]
+    path: ['/', '/auth/signin', '/auth/signup', /^\/loaderio/, '/auth/oauth/untappd']
   });
 }
 
